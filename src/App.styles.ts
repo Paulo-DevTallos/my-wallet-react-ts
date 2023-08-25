eb60bf82985f87d90a67b6af0d72ab54fbe61f8d
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-08-25 19:22:07 -0300

    feat: add base style for structure

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,14 +1,26 @@
 import styled from "styled-components";
 
-export const Container = styled.div`
-  background-color: #fff;
-`
+export const Container = styled.div``
 
 export const MainHeader = styled.header`
+  background-color: #344ED0;
+  color: #fff;
+  height: 150px;
+  text-align: center;
 `
 
 export const MainTitle = styled.h1`
+  padding-top: 30px;
+  letter-spacing: 1px;
 `
 
 export const Box = styled.main`
+  max-width: 980px;
+  margin: 0 auto 50px;
+  min-height: 600px;
+  background-color: #fff;
+  border-radius: 8px;
+  -webkit-box-shadow: -2px 2px 12px 0px rgba(0,0,0,0.35);
+  -moz-box-shadow: -2px 2px 12px 0px rgba(0,0,0,0.35);
+  box-shadow: -2px 2px 12px 0px rgba(0,0,0,0.35);
 `
