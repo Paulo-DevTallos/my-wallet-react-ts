b72734e838cd2860732d04a2630ae46e6c04a9ca
Paulo Sergio <paulo.sergio@tallos.com.br>
2023-08-25 19:10:31 -0300

    feat: create base styles

---
+++ src/App.styles.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  background-color: #fff;
+`
+
+export const MainHeader = styled.header`
+`
+
+export const MainTitle = styled.h1`
+`
+
+export const Box = styled.main`
+`
